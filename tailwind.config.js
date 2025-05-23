6250daf93a148e0b490a17d49152a5c798754f2c
Garima <garimarajput748@gmail.com>
2025-05-23 13:48:22 +0530

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3B82F6",
+        secondary: "#1E40AF",
+        accent: "#60A5FA",
+        dark: "#1F2937",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+};
